85ebc3fd4d9ce48f5e983094fa44c8671055f069
Werner Palfinger <werner.palfinger@gmail.com>
2024-08-31 14:41:26 +0200

    using _ separator to better support double names

--- jpg_to_xlsx.py
+++ jpg_to_xlsx.py
@@ -3,8 +3,8 @@ import PIL
 import openpyxl
 import openpyxl.drawing
 
-image_folder = Path("jpegs")
-outfilename = 'classlist.xlsx'
+image_folder = Path(r"C:\Users\werne\OneDrive - Erich Fried Realgymnasium\2C\Fotos-2C")
+outfilename = image_folder.parent/"Liste-2C.xlsx"
 row_height = 30
 image_height = 40
 
@@ -15,8 +15,9 @@ for i, fname in enumerate(image_folder.glob('*.jpg')):
     img.width,img.height = image_height*img.width/img.height,image_height
     sheet.add_image(img, f'B{i+1}')
     sheet.row_dimensions[i+1].height = row_height
-    parts = fname.stem.split()
+    parts = fname.stem.split('_')
     sheet.cell(row=i+1, column=1).value = int(parts[0])
-    sheet.cell(row=i+1, column=3).value = " ".join(parts[1:])
+    sheet.cell(row=i+1, column=3).value = parts[1]
+    sheet.cell(row=i+1, column=4).value = parts[2]
         
 workbook.save(outfilename)
--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -1,17 +1,18 @@
 import fitz
 from pathlib import Path
 
-pdf_file = "classlist_from_webuntis.pdf"
-image_folder = Path("jpgs")
+pdf_file = Path(r"C:\Users\werne\OneDrive - Erich Fried Realgymnasium\2C\Fotos-2C.pdf")
+image_folder = pdf_file.parent/pdf_file.stem
 
-image_folder.mkdir()
+image_folder.mkdir(exist_ok=True)
 pdf_document = fitz.open(pdf_file)
 for page in pdf_document:
     image_infos = {ii['xref']:ii['bbox'] for ii in page.get_image_info(xrefs=True)}
-    for xref,(x,y,w,h) in image_infos.items():
-        index = page.get_text("text", clip=[0,y,x,h]).strip()
-        last_name = page.get_text("text", clip=[x,y,220,h]).strip()
+    for i,(xref,(x,y,w,h)) in enumerate(image_infos.items()):
+        #index = i
+        index = int(page.get_text("text", clip=[0,y,x,h]).strip())
+        last_names = page.get_text("text", clip=[x,y,220,h]).strip()
         first_names = page.get_text("text", clip=[220,y,390,h]).strip()
-        with open(image_folder/f'{index} {first_names} {last_name}.jpg', "wb") as file:
+        with open(image_folder/f'{index:02d}_{first_names}_{last_names}.jpg', "wb") as file:
             file.write(pdf_document.extract_image(xref)["image"])
 
