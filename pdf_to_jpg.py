6e86ed01d874b58079bd261909de427f6ad1481a
Werner Palfinger <werner.palfinger@gmail.com>
2026-09-07 23:29:44 +0200

    import pymupdf, fixed index

--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -1,16 +1,17 @@
-import fitz
+import pymupdf
 from pathlib import Path
 
-pdf_file = Path(r"C:\Users\werne\OneDrive - Erich Fried Realgymnasium\2C\Fotos-2C.pdf")
+classname = "3B"
+pdf_file = Path(rf"C:\Users\werne\OneDrive - Erich Fried Realgymnasium\{classname}\Fotos-{classname}.pdf")
 image_folder = pdf_file.parent/pdf_file.stem
 
 image_folder.mkdir(exist_ok=True)
-pdf_document = fitz.open(pdf_file)
+pdf_document = pymupdf.open(pdf_file)
+index = 0
 for page in pdf_document:
     image_infos = {ii['xref']:ii['bbox'] for ii in page.get_image_info(xrefs=True)}
-    for i,(xref,(x,y,w,h)) in enumerate(image_infos.items()):
-        #index = i
-        index = int(page.get_text("text", clip=[0,y,x,h]).strip())
+    for xref,(x,y,w,h) in image_infos.items():
+        index = index + 1
         last_names = page.get_text("text", clip=[x,y,220,h]).strip()
         first_names = page.get_text("text", clip=[220,y,390,h]).strip()
         with open(image_folder/f'{index:02d}_{first_names}_{last_names}.jpg', "wb") as file:
